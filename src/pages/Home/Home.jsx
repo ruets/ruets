f27001ffd23181c81471c5476d9cc5e952429089
ruets <dev@ruets.pro>
2024-04-18 15:37:34 +0200

    feat(Home): add Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './Home.scss'
+import profilePic from "../../../public/profilePic.jpg";
+import { Link } from "react-router-dom";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="presentation-text">
+          <h1>Sébastien RUET</h1>
+          <h3>Développeur web et logiciel</h3>
+          <Link to="https://cvdesignr.com/p/6358f70dbc1fd">
+            <button className="cv-link">
+              <span>
+                CV
+              </span>
+            </button>
+          </Link>
+        </div>
+        <img className="profile-pic" src={profilePic}></img>
+      </>
+    )
+  }
+}
+
+export default Home;
