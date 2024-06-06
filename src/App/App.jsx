9c9c2ed017c26243183342227501c25837bd8ff4
ruets <dev@ruets.pro>
2024-06-06 05:56:03 +0200

    feat(about): add the About page

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.scss'
 import Navbar from '../components/Navbar/Navbar.jsx'
 import Home from '../pages/Home/Home.jsx'
+import About from '../pages/About/About.jsx'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   )
