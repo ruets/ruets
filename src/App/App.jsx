9df4b0fad169243a2d21f51b93fb37de2ecfdabb
ruets <dev@ruets.pro>
2024-06-06 05:12:29 +0200

    feat(navbar): add navbar

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.scss'
+import Navbar from '../components/Navbar/Navbar.jsx'
 import Home from '../pages/Home/Home.jsx'
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.scss';
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isChecked: false }; // Initialize checkbox state
+    console.log(this)
+  }
+
+  toggleCheckbox = () => { this.setState({ isChecked: !this.state.isChecked }) }
+
+  render() {
+    return (
+      <div className='navbar'>
+        <div className='navbar-toggle'>
+          <input type='checkbox' id='checkbox' checked={this.state.isChecked} onChange={this.toggleCheckbox}/>
+          <label for='checkbox' className='toggle'>
+            <div className='bars' id='bar1'/>
+            <div className='bars' id='bar2'/>
+            <div className='bars' id='bar3'/>
+          </label>
+        </div>
+
+        {this.state.isChecked && (
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">
+                  <button>
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+                    <span className="material-symbols-outlined icon">home</span>
+                    <span className="link-text">Accueil</span>
+                  </button>
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Navbar;
