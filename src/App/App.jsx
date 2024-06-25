96a3774cdd11f49f1a441cd8387b64e63aa12b0b
ruets <dev@ruets.pro>
2024-06-25 22:14:44 +0200

    feat(experiences): add Experiences page

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,6 +3,7 @@ import './App.scss'
 import Navbar from '../components/Navbar/Navbar.jsx'
 import Home from '../pages/Home/Home.jsx'
 import About from '../pages/About/About.jsx'
+import Experiences from '../pages/Experience/Experiences.jsx'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/experiences" element={<Experiences />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/icons/SchoolIcon.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class SchoolIcon extends React.Component {
+  render() {
+    return (
+      <div className="work-icon">
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
+        <span className="material-symbols-outlined icon">school</span>
+      </div>
+    );
+  }
+}
+
+export default SchoolIcon;
