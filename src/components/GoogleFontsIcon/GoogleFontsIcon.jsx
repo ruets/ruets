0fbf05e6d88cc6691c7eb7d139cb9ba9e5557e8c
ruets <dev@ruets.pro>
2024-06-27 10:46:02 +0200

    feat(timelineelement): add possibility to specify a link for timeline elements

--- src/components/GoogleFontsIcon/GoogleFontsIcon.jsx
+++ src/components/GoogleFontsIcon/GoogleFontsIcon.jsx
@@ -9,7 +9,7 @@ class GoogleFontsIcon extends React.Component {
     const { iconName } = this.props;
 
     return (
-      <div className="{iconName}-icon">
+      <div className={`${iconName}-icon`}>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
