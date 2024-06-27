f2749d5a558f0a0e88cd6e87a903980d70bc2c7a
ruets <dev@ruets.pro>
2024-06-27 09:02:26 +0200

    refactor(icons): delete old icons and replace with a better icon component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Navbar.scss';
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import GoogleFontsIcon from "../GoogleFontsIcon/GoogleFontsIcon";
+import "./Navbar.scss";
 
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
     this.state = { isChecked: false }; // Initialize checkbox state
-    console.log(this)
   }
 
-  toggleCheckbox = () => { this.setState({ isChecked: !this.state.isChecked }) }
+  toggleCheckbox = () => {
+    this.setState({ isChecked: !this.state.isChecked });
+  };
 
   render() {
     return (
-      <div className='navbar'>
-        <div className='navbar-toggle'>
-          <input type='checkbox' id='checkbox' checked={this.state.isChecked} onChange={this.toggleCheckbox}/>
-          <label for='checkbox' className='toggle'>
-            <div className='bars' id='bar1'/>
-            <div className='bars' id='bar2'/>
-            <div className='bars' id='bar3'/>
+      <div className="navbar">
+        <div className="navbar-toggle">
+          <input
+            type="checkbox"
+            id="checkbox"
+            checked={this.state.isChecked}
+            onChange={this.toggleCheckbox}
+          />
+          <label htmlFor="checkbox" className="toggle">
+            <div className="bars" id="bar1" />
+            <div className="bars" id="bar2" />
+            <div className="bars" id="bar3" />
           </label>
         </div>
 
@@ -29,8 +37,7 @@ class Navbar extends React.Component {
               <li>
                 <NavLink to="/">
                   <button>
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-                    <span className="material-symbols-outlined icon">home</span>
+                    <GoogleFontsIcon iconName="home" />
                     <span className="link-text">Accueil</span>
                   </button>
                 </NavLink>
@@ -38,8 +45,7 @@ class Navbar extends React.Component {
               <li>
                 <NavLink to="/about">
                   <button>
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-                    <span className="material-symbols-outlined icon">badge</span>
+                    <GoogleFontsIcon iconName="badge" />
                     <span className="link-text">A propos</span>
                   </button>
                 </NavLink>
@@ -47,12 +53,11 @@ class Navbar extends React.Component {
               <li>
                 <NavLink to="/experiences">
                   <button>
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-                    <span className="material-symbols-outlined icon">work</span>
+                    <GoogleFontsIcon iconName="work" />
                     <span className="link-text">Expériences</span>
                   </button>
                 </NavLink>
-            </li>
+              </li>
             </ul>
           </nav>
         )}
--- src/components/TimelineElement/TimelineElement.jsx
+++ src/components/TimelineElement/TimelineElement.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import { VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 
-import WorkIcon from "../icons/WorkIcon.jsx";
-import SchoolIcon from "../icons/SchoolIcon.jsx";
+import GoogleFontsIcon from "../GoogleFontsIcon/GoogleFontsIcon";
 import "./TimelineElement.scss";
 
 const TimelineElementType = {
-  WORK: "Work",
-  EDUCATION: "Education",
+  WORK: "work",
+  EDUCATION: "school",
 };
 
 class TimelineElement extends React.Component {
@@ -26,9 +25,7 @@ class TimelineElement extends React.Component {
         contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
         date={date}
         iconStyle={{ background: "#484848", color: "#fff" }}
-        icon={
-          type === TimelineElementType.EDUCATION ? <SchoolIcon /> : <WorkIcon />
-        }
+        icon={<GoogleFontsIcon iconName={type} />}
       >
         <h3 className="vertical-timeline-element-title">{title}</h3>
         <h4 className="vertical-timeline-element-subtitle">{society}</h4>
